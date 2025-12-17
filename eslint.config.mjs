01de82ab10d6016f93bf3309dabd8a8a68ab2180
Vadim Golovachev <v.golovachev@tinkoff.ru>
2025-12-17 17:52:57 +0400

    feat: project settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import nx from '@nx/eslint-plugin';
+import eslintPluginImportX from 'eslint-plugin-import-x';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   ...nx.configs['flat/base'],
@@ -26,6 +28,37 @@ export default [
     },
   },
   {
+    files: ['**/*.ts'],
+    plugins: {
+      'import-x': eslintPluginImportX,
+    },
+    settings: {
+      'import-x/parsers': {
+        '@typescript-eslint/parser': ['.ts'],
+      },
+      'import-x/resolver': {
+        typescript: {
+          project: ['tsconfig.base.json'],
+          alwaysTryTypes: true,
+        },
+      },
+    },
+    rules: {
+      'import-x/no-cycle': 'error',
+      'import-x/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          named: true,
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  {
     files: [
       '**/*.ts',
       '**/*.tsx',
@@ -37,6 +70,19 @@ export default [
       '**/*.mjs',
     ],
     // Override or add rules here
-    rules: {},
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/member-ordering': 'error',
+    },
   },
+  {
+    files: ['**/*.html'],
+    rules: {
+      '@angular-eslint/template/attributes-order': ['error'],
+    },
+  },
+  eslintConfigPrettier,
 ];
